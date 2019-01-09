5c1e1b3bca7bc26c60d634baf8764e3d21efb495
Samuel Chamberland <sam.chamberland@me.com>
2019-01-09 18:44:10 -0500

    Finalize script and update version

--- README.md
+++ README.md
@@ -1,42 +1,19 @@
 # create-expo-app
 
+A somewhat opinionated way to create React Native apps using Expo
+
 ## Usage
 
-_The commands have to be executed manually until process is automated_
-
-Initialize a blank Expo project →
-
 ```sh
-expo init --yarn --template blank YOUR_PROJECT_NAME
+create-expo-app <app-name>
 ```
 
-Add dev dependencies →
+## Includes
 
-```sh
-yarn add --dev typescript tslint tslint-react tslint-config-prettier prettier
-yarn add --dev jest jest-expo jest-fetch-mock ts-jest react-test-renderer react-native-testing-library
-yarn add --dev @types/expo @types/jest @types/react @types/react-native
-```
-
-Copy the configuration files located in the `template` directory →
-
-`copy tsconfig.json`
-
-`copy tslint.json`
-
-`copy .prettierrc`
-
-Replace your `scripts` object inside `package.json` →
-
-```json
-"scripts": {
-  "lint": "tslint --config tslint.json --project tsconfig.json",
-  "prettier": "prettier --write './src/**/*'",
-  "test": "jest",
-  "tsc": "tsc --noEmit"
-}
-```
-
-Copy the Jest configuration file (`template/jest.config.js`) and setup file (`template/jest/setup.ts`)
-
-Complete the project directories/file structure so it matches the `template` directory →
+- Expo
+- TypeScript
+- TSLint
+- Prettier
+- Jest
+- react-native-testing-library
+- A minimal directories structure
--- bin/create-expo-app.js
+++ bin/create-expo-app.js
@@ -1,76 +1,79 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const program = require('commander');
 const execa = require('execa');
+const ora = require('ora');
 
 program
-  .version('0.0.1')
-  .usage('<app>')
+  .version('1.0.0')
+  .usage('<app-name>')
   .arguments('<app>')
   .option('-s, --silent', 'scaffold app without printing to stdout')
-  .action(app => {
-    create(app);
-  })
+  .action(app => create(app))
   .parse(process.argv);
 
-const dependencies = [];
 const devDepencendies = [
-  'typescript',
-  'tslint',
-  'tslint-react',
-  'tslint-config-prettier',
-  'prettier',
-  'jest',
-  'jest-expo',
-  'jest-fetch-mock',
-  'ts-jest',
-  'react-test-renderer',
-  'react-native-testing-library',
   '@types/expo',
   '@types/jest',
-  '@types/react',
   '@types/react-native',
+  '@types/react',
+  'jest-expo',
+  'jest-fetch-mock',
+  'jest',
+  'prettier',
+  'react-native-testing-library',
+  'react-test-renderer',
+  'ts-jest',
+  'tslint-config-prettier',
+  'tslint-react',
+  'tslint',
+  'typescript',
 ];
+const pkgScripts = {
+  lint: 'tslint --config tslint.json --project tsconfig.json',
+  prettier: "prettier --write './src/**/*'",
+  test: 'jest',
+  tsc: 'tsc --noEmit',
+};
 
 async function create(app) {
-  try {
-    const currentDirectory = process.cwd();
-    const targetDirectory = path.resolve(process.cwd(), app);
+  const spinner = ora('Creating Expo app');
 
-    console.log('Creating new Expo app', targetDirectory);
-    const { stdout } = await execa('expo', [
-      'init',
-      '--yarn',
-      '--template',
-      'blank',
-      app,
-    ]);
-    console.log(
-      'Installing devDepencies (e.g. typescript, tslint, prettier, ...)',
-    );
+  try {
+    const targetDirectory = path.resolve(process.cwd(), app);
+    const currentDirectory = process.cwd();
+
+    spinner.start();
+    await execa('expo', ['init', '--yarn', '--template', 'blank', app]);
+    spinner.text =
+      'Installing devDependencies (e.g. typescript, tslint, prettier, jest, ...)';
     await execa('yarn', ['add', '--dev', ...devDepencendies], {
       cwd: targetDirectory,
     });
-    console.log(
-      'Creating configuration files (e.g. tsconfig.json, tslint.json, ...)',
+    spinner.text =
+      'Setting up project structure and creating configuration files';
+    await fs.copy(
+      path.resolve(currentDirectory, 'template'),
+      path.resolve(targetDirectory),
     );
-    fs.copyFileSync(
-      path.resolve(currentDirectory, 'template', 'tsconfig.json'),
-      path.resolve(targetDirectory, 'tsconfig.json'),
+    spinner.text = 'Editing scripts';
+    // edit `package.json` to include our scripts
+    const pkg = await fs.readJSON(
+      path.resolve(targetDirectory, 'package.json'),
     );
-    fs.copyFileSync(
-      path.resolve(currentDirectory, 'template', 'tslint.json'),
-      path.resolve(targetDirectory, 'tslint.json'),
+    await fs.writeJSON(
+      path.resolve(targetDirectory, 'package.json'),
+      { ...pkg, scripts: pkgScripts },
+      { spaces: 2 },
     );
-    fs.copyFileSync(
-      path.resolve(currentDirectory, 'template', '.prettierrc'),
-      path.resolve(targetDirectory, '.prettierrc'),
-    );
-    console.log(stdout);
+    spinner.text = 'Done!';
+    spinner.succeed();
   } catch (e) {
-    console.log('Something went wrong', e);
+    spinner.fail();
+    console.log('Something went wrong!');
+    console.log(e.stderr || e);
     process.exit(1);
   }
 }
