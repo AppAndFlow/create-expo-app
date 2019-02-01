d9ee2cf023ea695b09b0aad430479be298b5bd96
Samuel Chamberland <sam.chamberland@me.com>
2019-02-01 12:44:08 -0500

    Add more constants to metrics

--- template/src/constants/metrics.ts
+++ template/src/constants/metrics.ts
@@ -1,10 +1,26 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
 
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+const { width, height } = Dimensions.get('window');
+const makeHitSlop = (size: number) => ({
+  top: size,
+  right: size,
+  bottom: size,
+  left: size,
+});
+const makeCircle = (diameter: number) => ({
+  height: diameter,
+  width: diameter,
+  borderRadius: diameter / 2,
+});
 
 export default {
   width,
   height,
-  isSmallDevice: width < 375,
+  paddingHorizontal: 30,
+  isSmallDevice: width <= 375,
+  isIphoneX: height >= 800,
+  isIos: Platform.OS === 'ios',
+  isAndroid: Platform.OS === 'android',
+  makeHitSlop,
+  makeCircle,
 };
