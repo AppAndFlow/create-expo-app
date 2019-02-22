a84a4d59ce242dcb2310ff83446042798537f247
Samuel Chamberland <sam.chamberland@me.com>
2019-02-22 15:59:05 -0500

    Refactor metrics helpers

--- template/src/constants/metrics.ts
+++ template/src/constants/metrics.ts
@@ -14,13 +14,16 @@ const makeCircle = (size: number) => ({
 });
 
 export default {
-  windowWidth,
-  windowHeight,
-  paddingHorizontal: 30,
-  isSmallDevice: windowWidth < 375,
-  isIphoneX: windowHeight >= 800,
-  isIos: Platform.OS === 'ios',
+  headerHeight: 100,
   isAndroid: Platform.OS === 'android',
-  makeHitSlop,
+  isIos: Platform.OS === 'ios',
+  isIphoneX:
+    Platform.OS === 'ios' && (windowHeight === 812 || windowHeight === 896),
+  isSmallDevice: windowWidth < 375,
   makeCircle,
+  makeHitSlop,
+  paddingHorizontal: 20,
+  tabBarHeight: 60,
+  windowHeight,
+  windowWidth,
 };
