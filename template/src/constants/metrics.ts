953bd180bbe1b0db5ba86660aea599322f943b69
Samuel Chamberland <sam.chamberland@me.com>
2019-05-13 11:12:59 -0400

    Update metrics

--- template/src/constants/metrics.ts
+++ template/src/constants/metrics.ts
@@ -1,28 +1,29 @@
 import { Dimensions, Platform } from 'react-native';
+import { Constants } from 'expo';
 
 const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
-const makeHitSlop = (size: number) => ({
+const createHitSlop = (size: number) => ({
   top: size,
   right: size,
   bottom: size,
   left: size,
 });
-const makeCircle = (size: number) => ({
-  height: size,
-  width: size,
-  borderRadius: size / 2,
+const createCircle = (diameter: number) => ({
+  height: diameter,
+  width: diameter,
+  borderRadius: diameter / 2,
 });
 
 export default {
-  headerHeight: 100,
+  headerHeight: 110,
   isAndroid: Platform.OS === 'android',
   isIos: Platform.OS === 'ios',
   isIphoneX:
     Platform.OS === 'ios' && (windowHeight === 812 || windowHeight === 896),
   isSmallDevice: windowWidth < 375,
-  makeCircle,
-  makeHitSlop,
-  paddingHorizontal: 20,
+  createCircle,
+  createHitSlop,
+  statusBarHeight: Constants.statusBarHeight,
   tabBarHeight: 60,
   windowHeight,
   windowWidth,
