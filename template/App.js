5466ff2f37c214960b990b45ffab7bc1727862ac
Samuel Chamberland <sam.chamberland@me.com>
2019-10-09 10:39:52 -0400

    Allow LayoutAnimation to work on Android devices

--- template/App.js
+++ template/App.js
@@ -1,3 +1,12 @@
+import { UIManager } from 'react-native';
+
 import App from './src/components/App';
+import metrics from './src/constants/metrics';
+
+if (metrics.isAndroid) {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
 
 export default App;
